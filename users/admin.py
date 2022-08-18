6487d3cae9ff6b5598d611be7f877ce71586a5b7
Vu Tran <vu.trandang@amela.vn>
2022-08-18 09:16:57 +0700

    - Add admin site users

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth.admin import GroupAdmin, UserAdmin
-from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin
 
 from users.models import User
 
 admin.site.register(User, UserAdmin)
-admin.site.register(Group, GroupAdmin)
