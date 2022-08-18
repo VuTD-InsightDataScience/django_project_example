01e1891d1045e503b28215fa5ead785340c29edf
Vu Tran <vu.trandang@amela.vn>
2022-08-18 08:26:33 +0700

    - Add user for example

--- settings/__init__.py
+++ settings/__init__.py
@@ -59,7 +59,6 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
