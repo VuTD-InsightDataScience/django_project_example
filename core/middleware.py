c895ca11b346fffaf612b51b0c67b930a7483949
Vu Tran <vu.trandang@amela.vn>
2022-08-18 11:29:58 +0700

    Add decorators & mixins

--- core/middleware.py
+++ core/middleware.py
@@ -6,23 +6,21 @@ from django.utils.deprecation import MiddlewareMixin
 
 class WhoDidMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        user = None
         if hasattr(request, 'user') and request.user.is_authenticated:
-            user = request.user
-        if request.method in ('POST', 'PUT', 'PATCH'):
-            mark_who_did = partial(self.mark_who_did, user)
-            signals.pre_save.connect(
-                mark_who_did,
-                dispatch_uid=(self.__class__, request, 'pre',),
-                weak=False
-            )
-        if request.method in ('DELETE',):
-            mark_who_deleted = partial(self.mark_who_deleted, user)
-            signals.pre_delete.connect(
-                mark_who_deleted,
-                dispatch_uid=(self.__class__, request, 'pre',),
-                weak=False
-            )
+            if request.method in ('POST', 'PUT', 'PATCH'):
+                mark_who_did = partial(self.mark_who_did, request.user)
+                signals.pre_save.connect(
+                    mark_who_did,
+                    dispatch_uid=(self.__class__, request, 'pre',),
+                    weak=False
+                )
+            if request.method in ('DELETE',):
+                mark_who_deleted = partial(self.mark_who_deleted, request.user)
+                signals.pre_delete.connect(
+                    mark_who_deleted,
+                    dispatch_uid=(self.__class__, request, 'pre',),
+                    weak=False
+                )
 
     @staticmethod
     def mark_who_did(user, sender, instance, **kwargs):
