1ac65f1e307ebe60a9974c8f744548d99a1c39ee
Vu Tran <vu.trandang@amela.vn>
2022-08-22 11:42:29 +0700

    - Test bump version

--- example_app/views.py
+++ example_app/views.py
@@ -1,20 +1,5 @@
-import openpyxl
 from django.http import HttpResponse
 
 
 def excel_formula(request):
-    wb = openpyxl.load_workbook('Book1.xlsx')
-    ws = wb['Sheet1']
-    print('##########')
-    print(ws['A1'].data_type)
-    print(ws['A1'].value)
-    print('##########')
-    print('##########')
-    print(ws['G1'].data_type)
-    print(ws['G1'].value)
-    print('##########')
-    print('##########')
-    print(ws['F1'].data_type)
-    print(ws['F1'].value)
-    print('##########')
     return HttpResponse()
